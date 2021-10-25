3ff644f1f3437556c458d1a800150db2b350635d
black <black@example.com>
2021-10-25 14:17:32 +0500

    Edited Main Greeting

--- helloApp/Program.cs
+++ helloApp/Program.cs
@@ -6,7 +6,7 @@ namespace helloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
             Console.ReadLine();
         }
 
